8b62ecd16a6c4d5afd5526cf07130990081fc33b
sarahpng <sarahwaseem21@gmail.com>
2025-02-23 22:57:24 +0500

    routes setup

---
+++ lib/utils/routes/routes.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_with_provider/utils/routes/routes_name.dart';
+import 'package:mvvm_with_provider/view/home_screen.dart';
+import 'package:mvvm_with_provider/view/login_screen.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings setting) {
+    switch (setting.name) {
+      case RoutesName.home:
+        return MaterialPageRoute(
+            builder: (BuildContext context) => HomeScreen());
+
+      case RoutesName.login:
+        return MaterialPageRoute(
+            builder: (BuildContext context) => LoginScreen());
+
+      default:
+        return MaterialPageRoute(builder: (_) {
+          return Scaffold(
+            body: Center(
+              child: Text('No route defined'),
+            ),
+          );
+        });
+    }
+  }
+}
