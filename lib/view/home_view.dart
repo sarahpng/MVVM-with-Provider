4821053b55820be6a57ae79833fcc34eb7ff4a55
sarahpng <sarahwaseem21@gmail.com>
2025-03-02 23:23:36 +0500

    splash view and methods for user preference

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_with_provider/utils/routes/routes_name.dart';
+import 'package:mvvm_with_provider/view_model/user_view_model.dart';
+import 'package:provider/provider.dart';
 // import 'package:mvvm_with_provider/utils/routes/routes_name.dart';
-import 'package:mvvm_with_provider/utils/utils.dart';
+// import 'package:mvvm_with_provider/utils/utils.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -12,16 +15,21 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
+    final userPreference = Provider.of<UserViewModel>(context);
     return Scaffold(
-      body: Column(
-        children: [
-          TextButton(
-            onPressed: () {
-              Utils.flushbarErrorMessages('message', context);
-            },
-            child: Text('go to login'),
-          ),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            InkWell(
+                onTap: () {
+                  userPreference.remove().then((value) {
+                    Navigator.pushNamedAndRemoveUntil(
+                        context, RoutesName.login, (route) => false);
+                  });
+                },
+                child: Text('Home Screen')),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/view_model/services/splash_services.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:mvvm_with_provider/model/user_model.dart';
+import 'package:mvvm_with_provider/utils/routes/routes_name.dart';
+import 'package:mvvm_with_provider/view_model/user_view_model.dart';
+
+class SplashServices {
+  Future<UserModel> getUserData() => UserViewModel().getUser();
+
+  void checkAuthentication(BuildContext context) async {
+    getUserData().then((value) async {
+      if (value.token == 'null' || value.token == '') {
+        print(value.token);
+        await Future.delayed(Duration(seconds: 3));
+        Navigator.pushNamedAndRemoveUntil(
+            context, RoutesName.login, (route) => false);
+      } else {
+        await Future.delayed(Duration(seconds: 3));
+        Navigator.pushNamedAndRemoveUntil(
+            context, RoutesName.home, (route) => false);
+      }
+    }).onError((error, stackTrace) {
+      if (kDebugMode) {
+        print(error.toString());
+      }
+    });
+  }
+}
